0cf37fb020acb3461b6c16f60c52ee607d21d78a
aldrinsangga <aldrintenajasangga1995@gmail.com>
2026-06-18 12:24:40 +0800

    s

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import yts from "yt-search";
 import cors from "cors";
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(cors());
 app.use(express.json());
